1bdd6127eac24738ccbc5db864cccf900cfa05fd
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-09 23:36:57 -0800

    Adding different type of validations

--- src/main/java/rrsn/me/springbootvalidation/model/User.java
+++ src/main/java/rrsn/me/springbootvalidation/model/User.java
@@ -1,8 +1,31 @@
 package rrsn.me.springbootvalidation.model;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 public class User {
+    @NotBlank(message = "username cannot be empty")
+    private String name;
+    @NotBlank(message = "password cannot be empty")
+    @Length(min=6, max=10, message="length of password should between 6 to 10 characters")
+    private String password;
+    @Min(value=0)
+    @Max(value=100)
+    private Double grade;
+    @Email
+    private String email;
+
+    public User(String name, String password, Double grade, String email) {
+        this.name = name;
+        this.password = password;
+        this.grade = grade;
+        this.email = email;
+    }
+
     public String getName() {
         return name;
     }
@@ -27,15 +50,11 @@ public class User {
         this.grade = grade;
     }
 
-    @NotBlank(message = "username cannot be empty")
-    private String name;
-    @NotBlank(message = "password cannot be empty")
-    private String password;
-    private Double grade;
+    public String getEmail() {
+        return email;
+    }
 
-    public User(String name, String password, Double grade) {
-        this.name = name;
-        this.password = password;
-        this.grade = grade;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
